21516592fcdc62e003ac634349dcefcf79019a09
王志刚 <wang.zhigang@ss-cn-wzg.home.e-kmall.com>
2017-09-05 13:26:43 +0800

    start service cron

--- shell/run.sh
+++ shell/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+service cron start
+
 if [ -z "$ETCD_URL" ]; then
 	nginx -g 'daemon off;'
 else
-    
     # Put a continual polling `confd` process into the background to watch
     # for changes every 10 seconds
 	confd -interval 10 -backend $BACKEND_KIND -node $ETCD_URL -config-file /etc/confd/conf.d/nginx.toml >> /var/log/confd.log 2>&1 &
